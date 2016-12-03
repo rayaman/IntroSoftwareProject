c47f1892f8085ad97190daf5d2dd3e408c0051bc
Ryan Ward <rayaman99@gmail.com>
2016-12-03 00:43:42 -0500

    Add files via upload

--- server.lua
+++ server.lua
@@ -3,4 +3,7 @@ require("Libs/MultiManager") -- allows for multitasking
 require("net") -- Loads the networking library
 require("net.chatting") -- loads the networking chatting module
 server=net:newTCPServer(12345) -- starts a server with the port 12345 on local host
+server.OnChatRecieved(function(struct) -- where we handle users and messages
+	print(struct.user..": "..struct.msg)
+end)
 multi:mainloop() -- starts the mainloop to keep the server going
